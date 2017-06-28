d4b9fd3632c323a26dc60c55e4a6b0e9352f3acf
nhpd <dmitry.klpv@gmail.com>
2017-06-28 16:00:40 +0300

    Return if no Gemfile.lock in the directory exists

--- gemupdate.gemspec
+++ gemupdate.gemspec
@@ -22,7 +22,7 @@ Gem::Specification.new do |s|
   # end
 
   # todo: revisit this later
-  s.required_ruby_version     = ">= 2.4.0"
+  s.required_ruby_version = ">= 2.4.0"
 
   s.add_development_dependency "bundler", "~> 1.15"
 
--- lib/gemupdate.rb
+++ lib/gemupdate.rb
@@ -3,7 +3,18 @@ require 'bundler'
 class Gemupdate
 
   def self.run(gemfile_lock_path)
-    lockfile = Bundler::LockfileParser.new(Bundler.read_file(gemfile_lock_path))
+
+    unless File.exists?(gemfile_lock_path)
+      puts "No Gemfile in that directory"
+      return
+    end
+
+    lockfile_raw = Bundler.read_file(gemfile_lock_path)
+    parse_lockfile(lockfile_raw)
+  end
+
+  def self.parse_lockfile(lockfile_raw)
+    lockfile = Bundler::LockfileParser.new(lockfile_raw)
 
     lockfile.specs.each do |s|
       puts "Spec: #{s}"
